38d3d2f7a33b69ee618419bb6ae32d2635b46367
DarshanCHMSR <chdarshan99@gmail.com>
2025-04-07 23:41:00 +0530

    the bucket

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -7,7 +7,7 @@ dotenv.config();
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "ap-south-1",
+  region: "eu-north-1",
 });
 const s3 = new AWS.S3();
 
