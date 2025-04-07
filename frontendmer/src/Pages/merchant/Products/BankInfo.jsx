54b6ac7fcb9c001d2c83398e062993dea6f00665
DarshanCHMSR <chdarshan99@gmail.com>
2025-04-07 19:10:52 +0530

    the submit

--- frontendmer/src/Pages/merchant/Products/BankInfo.jsx
+++ frontendmer/src/Pages/merchant/Products/BankInfo.jsx
@@ -15,6 +15,7 @@ import ReactPaginate from 'react-paginate';
 const BankInfo = () => {
     const [loading, setLoading] = useState(false);
       const navigate = useNavigate();
+      const [senting,setSenting] = useState(false)
     
     const [formData, setFormData] = useState({
         accountNumber: "",
@@ -110,6 +111,7 @@ const BankInfo = () => {
       const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value.toUpperCase() });
     };
+    setSenting("true");
     const olduserName = parsedData.user.userName || "";
     const oldaccountNumber = parsedData.user.accountNumber || "";
     const oldifscCode = parsedData.user.ifscCode || "";
@@ -282,7 +284,8 @@ const BankInfo = () => {
           </div>
 
           <button type="submit" className="btn btn-primary w-100">
-            Submit
+          {senting ? "Submitting..." : "Submit"}
+
           </button>
         </form>
       </div>
