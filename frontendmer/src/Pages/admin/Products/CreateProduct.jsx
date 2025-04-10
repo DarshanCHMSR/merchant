e00bbebcdb8802c571537b509cadb8f3cf0916bb
DarshanCHMSR <164608140+DarshanCHMSR@users.noreply.github.com>
2025-04-10 09:17:34 +0530

    Update CreateProduct.jsx

--- frontendmer/src/Pages/admin/Products/CreateProduct.jsx
+++ frontendmer/src/Pages/admin/Products/CreateProduct.jsx
@@ -183,7 +183,7 @@ const CreateProduct = () => {
     const lastNumber = parseInt(lastId.split("-")[1]);
     const newNumber = (lastNumber + 1).toString().padStart(3, "0");
 
-    setId(`VK-${newNumber}`);
+    setId(`VKV-${newNumber}`);
     setIsidgenerated(true);
     setLoading(false);
   };
