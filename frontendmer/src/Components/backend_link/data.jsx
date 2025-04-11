2745fb72541b2e00c956828685de292aab26a3ed
DarshanCHMSR <chdarshan99@gmail.com>
2025-04-12 01:14:05 +0530

    the lambda

--- frontendmer/src/Components/backend_link/data.jsx
+++ frontendmer/src/Components/backend_link/data.jsx
@@ -1,2 +1,3 @@
-export const url  = 'http://localhost:5000'
+// export const url  = 'http://localhost:5000'
 // export const url ='https://merchant-2oqn.vercel.app'
+export const url = "https://eh9otewt3k.execute-api.eu-north-1.amazonaws.com/prostage"
