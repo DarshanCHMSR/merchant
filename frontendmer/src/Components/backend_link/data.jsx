8976e4125cf7d20b52d2630f7362bff26b921ded
DarshanCHMSR <chdarshan99@gmail.com>
2025-04-12 10:29:54 +0530

    the data

--- frontendmer/src/Components/backend_link/data.jsx
+++ frontendmer/src/Components/backend_link/data.jsx
@@ -1,3 +1,4 @@
 // export const url  = 'http://localhost:5000'
 // export const url ='https://merchant-2oqn.vercel.app'
-export const url = "https://eh9otewt3k.execute-api.eu-north-1.amazonaws.com/prostage"
+// export const url = "https://eh9otewt3k.execute-api.eu-north-1.amazonaws.com/prostage"
+export const url = "https://h61dj13gm7.execute-api.eu-north-1.amazonaws.com/pro"
