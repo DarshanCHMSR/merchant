2407999eefbea0925e0a6b5572e4e8ce484c187b
DarshanCHMSR <chdarshan99@gmail.com>
2025-05-03 23:14:49 +0530

    git public

--- categaries/routes/otpRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendotp,verifyotp } from "../controllers/otpController.js";
-
-const router = express.Router();
-// * otp routes
-router.post('/send-otp', sendotp);
-router.post('/verify-otp', verifyotp);
-
-export default router
